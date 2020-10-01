3996321310977a0d494c5652a8ecfdbf637bbe30
Amanda Ciobanu <amanda@ciobanu.org>
2020-10-01 09:44:01 +0100

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,10 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {:host => 'splah-amandaciobanu.herokuapp.com', :protocol => 'http'}
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
