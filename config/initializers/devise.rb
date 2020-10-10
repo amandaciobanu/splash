7436ce06e6eae567377aa08728e37971d2a92c91
Amanda Ciobanu <amanda@ciobanu.org>
2020-10-10 11:11:22 +0100

    feat: config send email in production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'amandatiemann@gmail.com'
+  config.mailer_sender = 'contact@sandboxdb3d48eb52384728a45d1c47c3354af6.mailgun.org'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
